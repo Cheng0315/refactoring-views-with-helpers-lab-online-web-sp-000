3fda29146a20ff7e78137847345613e1e2bf6f7c
Cheng Vue <csalvue@gmail.com>
2019-01-31 07:54:07 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
   end
 
   def artist_name=(name)
