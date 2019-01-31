dad173c648db168ab60252cf51599db1c04e92a2
Cheng Vue <csalvue@gmail.com>
2019-01-31 08:32:29 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,7 +5,8 @@ module ArtistsHelper
       link_to song.title, song_path(song)
       link_to song.artist.name, artist_path(song.artist)
     else
-
+      link_to song.title, song_path(song)
+      link_to 'Add Artist', edit_song_path(song)
     end
   end
 end
