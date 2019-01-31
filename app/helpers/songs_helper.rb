65e8ab0819d5878792282d308a5dadeab2a40756
Cheng Vue <csalvue@gmail.com>
2019-01-31 07:24:19 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,11 +2,11 @@ module SongsHelper
 
   def display_artist(song)
     if song.artist
-      link_to song.title, song_path(song)
-      link_to song.artist.name, artist_path(song.artist)
-      link_to "All Songs", songs_path
+      <h1><%= link_to @song.title, song_path(@song) %></h1>
+      <%= link_to @song.artist.name, artist_path(@song.artist) %>
+      <%= link_to "All Songs", songs_path %>
      else
-       link_to 'Add_Artist', edit_song_path(song)
+       <%= link_to @song.artist.name, artist_path(@song.artist) %>
      end
   end
 end
