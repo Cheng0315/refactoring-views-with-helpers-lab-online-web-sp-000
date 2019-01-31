e48df482fb47bf5314f0614b5cad436537e23d4e
Cheng Vue <csalvue@gmail.com>
2019-01-31 07:26:00 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,12 +1,3 @@
 module SongsHelper
 
-  def display_artist(song)
-    if song.artist
-      <h1><%= link_to song.title, song_path(song) %></h1>
-      <%= link_to song.artist.name, artist_path(song.artist) %>
-      <%= link_to "All Songs", songs_path %>
-     else
-       <%= link_to song.artist.name, artist_path(song.artist) %>
-     end
-  end
 end
