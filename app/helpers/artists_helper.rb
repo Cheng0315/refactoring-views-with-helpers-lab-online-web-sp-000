4669d89262b60361dc45289abe03846690cd37a1
Cheng Vue <csalvue@gmail.com>
2019-01-31 07:37:22 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,11 +2,11 @@ module ArtistsHelper
 
   def display_artist(song)
     if song.artist
-      "<h1><%= link_to @song.title, song_path(@song) %></h1><br>"
-      "<%= link_to @song.artist.name, artist_path(@song.artist) %><br>"
-      "<%= link_to 'All Songs', songs_path %><br>"
+       link_to @song.title, song_path(@song)
+       link_to @song.artist.name, artist_path(@song.artist)
+       link_to 'All Songs', songs_path
     else
-      "<%= link_to 'Add Artist', edit_song_path(@song) %><br>"
+       link_to 'Add Artist', edit_song_path(@song)
     end
   end
 end
