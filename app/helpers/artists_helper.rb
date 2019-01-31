932425401971b76802b5ef63a7895af38c6a9f15
Cheng Vue <csalvue@gmail.com>
2019-01-31 07:46:48 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,7 +4,6 @@ module ArtistsHelper
     if song.artist
        link_to song.title, song_path(song)
        link_to song.artist.name, artist_path(song.artist)
-       link_to 'All Songs', songs_path
     else
        link_to 'Add Artist', edit_song_path(song)
     end
